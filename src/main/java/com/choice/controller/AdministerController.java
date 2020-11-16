d569526b8ea41e2c5b68f7b8e8bfc3360b1cc8eb
chailong <2012126846@qq.com>
2020-11-16 22:16:42 +0800

    fisrt commit

---
+++ src/main/java/com/choice/controller/AdministerController.java
@@ -0,0 +1,5 @@
+package com.choice.controller;
+
+public class AdministerController {
+
+}
---
+++ src/main/java/com/choice/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.choice.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
