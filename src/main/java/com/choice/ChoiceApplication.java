3883dfab88903b8dbb94f4d874966f1bd18ad5e2
chailong <2012126846@qq.com>
2020-11-18 12:51:44 +0800

    chailong

--- src/main/java/com/choice/ChoiceApplication.java
+++ src/main/java/com/choice/ChoiceApplication.java
@@ -1,10 +1,14 @@
 package com.choice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.context.WebApplicationContext;
+
 @SpringBootApplication
 public class ChoiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChoiceApplication.class, args);
     }
-
 }
